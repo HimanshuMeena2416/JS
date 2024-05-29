945d2e743946419bb3ff7d496aa073bebf9c4839
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-29 11:15:22 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// //comparisions convert null to 0
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=0);
+
+// //equality don't convert null to 0
+// console.log(null==0);
+
+
+
+// //for undefined it will always be false 
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+
+
+
+//=== strict check: even check datatypes
+
+console.log("2"==2);
+console.log("2"===2);//false because datatype alag hai
