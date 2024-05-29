a2417e924d66f62add0212acb764200bad53cf0b
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-29 02:51:05 +0000

    changes made

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,19 @@
 const accountId = 144553;
-let accountEmail = "abc@gmail.com";
+let accountEmail = "abc@gmail.com"; //used now
 var accountPassword = "12345";
 accountCity = "Udaipur";
+let accountState;
+//accountId = 2;    //Not allowed
 
-accountId = 2;
+accountEmail = "abcd@gmail.com";
+accountPassword = "54321";
+accountCity = "Dungarpur";
+
+//prefer not to use var because of block scope and functional score
 
 console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountEmail, accountId, accountPassword, accountCity]);
+console.log(accountState);
