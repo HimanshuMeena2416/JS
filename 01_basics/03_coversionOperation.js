57027eacf06c914e65e44f5e93496d73e9244dc8
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-29 03:32:22 +0000

    conversion_operation

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc";
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(" ");
+
+let score2 = null;
+console.log(typeof score2);
+let valueInNumber2 = Number(score2);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+console.log(" ");
+
+let score3 = undefined;
+console.log(typeof score3);
+let valueInNumber3 = Number(score3);
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+console.log(" ");
+
+
+
+co
+// "33" => 33
+// "33abc" =>NaN
+// true=>1; flase=>0
