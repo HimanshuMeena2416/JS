c612f825a73c88f2f8fe8e7e81e9234f5b1acfad
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-29 10:55:37 +0000

    commit

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -1,23 +1,23 @@
-let score = "33abc";
-console.log(typeof score);
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-console.log(" ");
+// let score = "33abc";
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(" ");
 
-let score2 = null;
-console.log(typeof score2);
-let valueInNumber2 = Number(score2);
-console.log(typeof valueInNumber2);
-console.log(valueInNumber2);
-console.log(" ");
+// let score2 = null;
+// console.log(typeof score2);
+// let valueInNumber2 = Number(score2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+// console.log(" ");
 
-let score3 = undefined;
-console.log(typeof score3);
-let valueInNumber3 = Number(score3);
-console.log(typeof valueInNumber3);
-console.log(valueInNumber3);
-console.log(" ");
+// let score3 = undefined;
+// console.log(typeof score3);
+// let valueInNumber3 = Number(score3);
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+// console.log(" ");
 
 //conversion to number
 // "33" => 33
@@ -27,3 +27,14 @@ console.log(" ");
 //conversion to boolean
 // 1=>true;0=>false
 // ""=> false; "Hi"=>true
+
+
+// console.log(2**3);//power
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+
+// console.log(+true);//1
+// console.log(+"");//0
