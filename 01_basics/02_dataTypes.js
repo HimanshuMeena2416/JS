1e0cebca6acf1f935b2643514ef4ed0e4c169786
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-29 03:09:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+// alert("hello") //we are using node js not browsers
+console.log();
+
+let name = "Himanshu";
+let age = 19;
+let isLoggedIn = false;
+
+//number
+//bigint
+//string =>""
+//boolean => true/false
+//null=> standalone value. it means empty
+//undefined => when value not defined
+//symbol =>for uniqueness
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
