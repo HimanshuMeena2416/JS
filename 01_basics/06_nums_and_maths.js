2789de0807664ae6b352176ad33928a6adaa30a8
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-30 09:26:07 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString()); //changes num to string
+// console.log(balance.toFixed(2)); //2 digits after point
+
+// const otherNumer = 123.8966;
+// console.log(otherNumer.toPrecision(4)); //precise value of 4 digits..round off krke..string return krega
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString("en-IN")); //apni country ke accn digits aaygi
+
+//+++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3, 7, 2, 5));
+// console.log(Math.max(3, 7, 2, 5));
+
+console.log(Math.random()); //random value between 0 and 1
