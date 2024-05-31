ec038f6b24420b318bb2d008a96f91b965624a9f
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-31 14:44:00 +0000

    objects and array in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -21,5 +21,46 @@ function loginUserMessage(username = "raj") {
   return `${username} just logged in`;
 }
 
-console.log(loginUserMessage("Himz"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("Himz"));
+// console.log(loginUserMessage());
+
+// function calculateCartPrice(num1) {
+//   return num1;
+// }
+// console.log(calculateCartPrice(800, 300, 400)); //it will give 800 but we want all three
+
+// function calculateCartPrice(...num1) {
+//   return num1;
+// }
+// console.log(calculateCartPrice(800, 300, 400, 450, 440));// output an array with all values
+
+// function calculateCartPrice(num2, num3, ...num1) {
+//   return num1;
+// }
+// console.log(calculateCartPrice(800, 300, 400, 450, 440)); // output [400,450,440] because yhi hai num1 mai
+
+const user = {
+  username: "himz",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// handleObject(user);
+handleObject({
+  username: "raj",
+  price: 299,
+}); //object can be passed directly
+
+const myNewArray = [200, 400, 500, 99];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([345, 55, 643, 56]));
