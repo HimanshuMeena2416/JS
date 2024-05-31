ba883c612de270a461a298ed2e36d7f71a3c3e86
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-05-31 02:52:48 +0000

    array completed

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,26 @@
+const marvel_heroes = ["thor", "Ironman", "spiderman"];
+const dc_heroes = ["superman", "flash", "batman"];
+// marvel_heroes.push(dc_heroes); //array ke ander array push krega
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes); //concat return new array. 2 array se single array bn jayga
+// console.log(allHeroes);
+
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes]; //it is used mostly
+// console.log(all_new_heroes);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+// const real_another_array = another_array.flat(Infinity); //sbko single array me change kr dega
+// console.log(real_another_array);
+
+// console.log(Array.isArray("himz"));
+// console.log(Array.from("himz")); //used to make array from whatever given
+// console.log(Array.from({ name: "raju" })); //yaha humne bataya nhi ki key ka banana hai ya value ka islie empty array aayga
+// console.log(Array.from({ name: "raju", village: "Chhani" }.name));
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// console.log(Array.of(score1, score2, score3));
