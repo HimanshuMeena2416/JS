09cf7f4825223e6373180f986f9828ab4f21cdbf
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-06-03 02:38:19 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+//var c = 300; dont use var
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30; //ye block scope ke bahar nhi aana chahiye tha pr aa gya...islie let use kro
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
