94f88a137771f8f9910602d3737c84876f958b0d
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-06-03 11:25:37 +0000

    commit 1

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+//if
+
+// const isUserLoggedIn = true;
+// if (isUserLoggedIn) {
+// }
+
+// if (2 == "2") {
+//   //type check nhi hota
+//   console.log("Executed");
+// }
+
+// if (2 === "2") {
+//   //type bhi check ho jata yaha
+//   console.log("Executed");
+// }
+
+// const temperature = 55;
+// if (temperature < 50) {
+//   console.log("less than 50");
+// } else console.log("greater than 50");
+
+// const score = 200;
+
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`User power: ${power}`);
+// }
+
+const balance = 1000;
+
+if (balance > 500) console.log("test");
