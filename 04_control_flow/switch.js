df86a3646bf01ca3faf9524cd105012054d7d05e
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-06-03 15:27:29 +0000

    switch statement

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -5,3 +5,27 @@
 //   default:
 //     break;
 // }
+
+const month = 4;
+
+switch (month) {
+  case 1:
+    console.log("jan");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+
+  default:
+    console.log("fefault case");
+    break;
+}
