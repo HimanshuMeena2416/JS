89b35738596a718fb54437af06c25bb407cf01e4
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-06-04 05:01:08 +0000

    while do while

---
+++ 05_loops/two.js
@@ -0,0 +1,18 @@
+// let i = 0;
+// while (i <= 10) {
+//   console.log(`value of i is ${i}`);
+//   i = i + 2;
+// }
+
+// let myArray = ["flash", "batman", "superman"];
+// i = 0;
+// while (i < myArray.length) {
+//   console.log(`value is ${myArray[i]}`);
+//   i++;
+// }
+
+let score = 11;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
