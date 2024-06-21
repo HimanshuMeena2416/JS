e17282996c2183c15dcda0adefc106c7b82bb63e
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-06-21 17:23:58 +0000

    oops

---
+++ 10_OOPs/oop.js
@@ -0,0 +1,19 @@
+// const user = {
+//   username: "Himz",
+//   loginCount: 8,
+//   signIn: true,
+
+//   getUserdetails: function () {
+//     //console.log("Got user details from database");
+//     //console.log(`Username: ${this.username}`);
+//     console.log(this);
+//   },
+// };
+
+// console.log(user.loginCount);
+// user.getUserdetails();
+// //console.log(this);
+
+
+
+
