7483a38b4cc4d59934b52a5a5ba6915da1dcb9df
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-06-30 06:31:09 +0000

    Prototype

--- 10_OOPs/Prototype.js
+++ 10_OOPs/Prototype.js
@@ -1,26 +1,63 @@
-// let myName = "Himz    ";
+// // let myName = "Himz    ";
 
-// console.log(myName.truelength()); //ye hai nhi banakr inject krna hai taki sari strings ke sath use kr sku
+// // console.log(myName.truelength()); //ye hai nhi banakr inject krna hai taki sari strings ke sath use kr sku
 
-let myPlayers = ["Sachin", "Dhoni"];
+// let myPlayers = ["Sachin", "Dhoni"];
 
-let playerRole = {
-  Sachin: "anchor",
-  Dhoni: "hitter",
+// let playerRole = {
+//   Sachin: "anchor",
+//   Dhoni: "hitter",
 
-  getDhoniRole: function () {
-    console.log(`Mahi is ${this.Dhoni}`);
-  },
-};
+//   getDhoniRole: function () {
+//     console.log(`Mahi is ${this.Dhoni}`);
+//   },
+// };
 
-Object.prototype.himz = function () {
-  console.log(`Himz is omnipresent`);
-}; //humne himz naam ka function banakr object me inject kr diya taki sbhi use kr ske ise kyuki sbhi object hi toh hote hai
+// Object.prototype.himz = function () {
+//   console.log(`Himz is omnipresent`);
+// }; //humne himz naam ka function banakr object me inject kr diya taki sbhi use kr ske ise kyuki sbhi object hi toh hote hai
 
-playerRole.himz();
+// playerRole.himz();
 
-myPlayers.himz();
+// myPlayers.himz();
 
-Array.prototype.hibuddy = function () {
-  console.log(`testing...Array k lie banaya hai`);
-}; //inject function in all arrays to check wether it be be automatically injected to objects
+// Array.prototype.hibuddy = function () {
+//   console.log(`testing...Array k lie banaya hai`);
+// }; //inject function in all arrays to check wether it be be automatically injected to objects
+
+// //inheritance
+// //old approach
+// const User = {
+//   name: "himz",
+//   email: "123@gmail.com",
+// };
+// const Teacher = {
+//   makevideo: true,
+// };
+
+// const TeachingSupport = {
+//   isavailable: false,
+// };
+
+// const TASupport = {
+//   makeAssignment: "JS assignment",
+//   fullTime: ture,
+//   __proto__: TeachingSupport,
+// };
+
+// Teacher.__proto__ = User;
+
+// //modern syntax
+
+// Object.setPrototypeOf(TeachingSupport, Teacher);
+
+let anotherString = "Himzscorpion   ";
+
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  console.log(`True length is ${this.trim().length}`);
+};//string me naya function add kr lia
+
+anotherString.trueLength();
+
+"karan kaur".trueLength();
