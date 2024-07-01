1cdeba388fad10c803df8f26136222bad217952b
himanshu <59828352+HimanshuMeena2416@users.noreply.github.com>
2024-07-01 09:42:50 +0000

    getter setter

---
+++ 10_OOPs/getter_setter.js
@@ -0,0 +1,23 @@
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+  //getter setter sath likhne pdte hai
+  get email() {
+    return this._email.toUpperCase();
+  }
+  set email(value) {
+    this._email = value;
+  }
+  get password() {
+    return this._password.toUpperCase();
+  }
+  set password(value) {
+    this._password = value;
+  }
+}
+
+const user1 = new User("12@gmail.com", "abc");
+
+console.log(user1.email);
